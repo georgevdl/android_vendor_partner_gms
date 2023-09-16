acc869cade089d295d4242792b5b7949f1ba5c6c
George <6823761+georgevdl@users.noreply.github.com>
2023-09-16 21:22:24 +0300

    add Android Auto (Android 13)

---
+++ AndroidAutoStubPrebuilt/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.google.android.projection.gearhead.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := AndroidAutoStubPrebuilt
+LOCAL_SRC_FILES := AndroidAutoStubPrebuilt.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REQUIRED_MODULES := com.google.android.projection.gearhead.xml
+LOCAL_PRODUCT_MODULE := true
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
+include $(BUILD_PREBUILT)
+
